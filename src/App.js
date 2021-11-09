b24a93d016c2b0a3465303d93149fa946a65b5bb
samuel <samuel.martinez.morales@gmail.com>
2021-11-09 10:58:06 -0600

    Project starter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import ReviewDetails from "./pages/ReviewDetails"
+import Category from "./pages/ReviewDetails"
+import SiteHeader from "./components/SiteHeader";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SiteHeader />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/details/:id" element={<ReviewDetails />} />
+        <Route path="/category/:id" element={<Category />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ReviewDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviewdetails = () => {
+    return (
+        <div>
+            Details
+        </div>
+    );
+}
+
+export default Reviewdetails;
