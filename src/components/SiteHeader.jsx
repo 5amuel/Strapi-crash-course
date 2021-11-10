911ffc4cfad87db846f890955e03afb325553d25
samuel <samuel.martinez.morales@gmail.com>
2021-11-09 21:10:54 -0600

    Relational data from strapi UI, Adding categories to SiteHeader

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client';
+
+const CATEGORIES = gql`
+    query GetCategories{
+        categories{
+            name,
+            id
+        }
+    }
+`
+
 const SiteHeader = () => {
+    const {loading, error, data}=useQuery(CATEGORIES)
+    if(loading) return <p>loading...</p>
+    if(error) return <p>error...</p>
     return (
         <div className="site-header">
             <Link to="/"><h1>Ninja Reviews</h1></Link>
+            <nav className="categories">
+                <span>Filter reviews by category:</span>
+                {data.categories.map(category => (
+                    <Link key={category.id} to={`/category/${category.id}`}>{category.name}</Link>
+                ))}
+            </nav>
         </div>
     );
 }
